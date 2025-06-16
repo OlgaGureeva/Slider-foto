91a54325556a16fa3bf9972d79a4012ae87d745a
OlgaGureeva <gureeva.olya.04@mail.ru>
2025-06-16 18:29:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,13 @@ function initSlider() {
 
  let sliderImages = document.querySelector(".content-box__slider");
  let sliderArrows = document.querySelector(".content-box__arrows");
+ let sliderDots = document.querySelector(".content-box__dots");
+ let sliderTitle = document.querySelector(".content-box__titles");
 
  initImages();
  initArrows();
+ initDots();
+ initTitles();
 
  function initImages() {
     images.forEach((image, index) => {
@@ -45,6 +49,37 @@ function initArrows() {
             moveSlider(nextNumber);
         });
     });
+}
+
+function initDots() {
+    images.forEach((image, index) => {
+        let dot = `<div class="slider__dots-item n${index} ${index ===0? "active" : ""}" data-index="${index}"></div>`;
+        sliderDots.innerHTML += dot;
+    });
+    sliderDots.querySelectorAll(".slider__dots-item").forEach(dot => {
+        dot.addEventListener("click", function () {
+            moveSlider(this.dataset.index);
+            sliderDots.querySelector(".active").classList.remove("active");
+            this.classList.add("active");
+        })
+    })
+}
+
+function initTitles () {
+    sliderTitle.querySelectorAll(".titles__item").forEach(title => {
+        title.addEventListener("click", function() {
+            let curTitle = +sliderImages.querySelector(".active").dataset.index;
+            let nextTitle;
+            if (title.classList.contains("first")) {
+                nextTitle = curTitle ===0? images.length - 1 : curTitle - 1;
+            } else {
+                nextTitle = curTitle === images.length - 1? 0 : curTitle + 1;
+            }
+            moveSlider (nextTitle);
+
+        })
+    })
+        
 }
 
 function moveSlider (num) {
